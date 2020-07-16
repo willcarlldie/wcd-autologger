ce3ce386cd75d782e597c3804e3a48bd7ca48637
willcarlldie <67031687+willcarlldie@users.noreply.github.com>
2020-07-16 02:51:51 +0000

    Update wcd-autologger.lua

--- wcd-autologger.lua
+++ wcd-autologger.lua
@@ -1,6 +1,7 @@
 if (LoggingCombat()) then
-    DEFAULT_CHAT_FRAME:AddMessage("Combat is already being logged");
+    DEFAULT_CHAT_FRAME:AddMessage("Combat Logging is on");
   else
-    DEFAULT_CHAT_FRAME:AddMessage("Combat is not being logged - starting it!");  
+    DEFAULT_CHAT_FRAME:AddMessage("Combat Logging is not started - starting the scribes!");  
     LoggingCombat(1);
+    DEFAULT_CHAT_FRAME:AddMessage("Combat Logging is on");  
   end
